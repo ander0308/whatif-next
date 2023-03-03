4169da9313a4867c691ac32d71de1f4c3a903dde
Anderson Santana <ander0308@yahoo.com.br>
2023-03-03 16:34:57 -0300

    feat: head title in components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,10 @@
 import ComponentPageNotFound from '@/components/pageNotFound'
+import PageTitle from '@/components/PageTitle'
 
 function PageNotFound() {
   return (
     <>
+        <PageTitle title="What if? | Página não encontrada" description="Um projeto desenvolvindo no curso codeboost"/>
       <ComponentPageNotFound />
     </>
   )
--- src/pages/character/[slug].js
+++ src/pages/character/[slug].js
@@ -1,10 +1,12 @@
 import SectionHeroCharacter from "@/components/details/sectionHeroCharacter"
 import ListCharacters from "@/components/home/ListCharacters"
+import PageTitle from "@/components/PageTitle"
 
 function PageCharacter() {
 
   return (
     <>
+      <PageTitle title="Whatif | Detalhes do personagem" description="Um projeto desenvolvindo no curso codeboost"/>
       <SectionHeroCharacter />
       <ListCharacters />
     </>
